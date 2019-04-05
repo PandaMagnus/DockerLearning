522120106414dcb6534fc2373ffdee6cd2274a16
Mike Curn <mike.curn@intellitect.com>
2019-04-04 22:01:38 -0700

    Some different attempts at getting the dockerfile and tests working

--- myWebApp.Tests/SmokeTests.cs
+++ myWebApp.Tests/SmokeTests.cs
@@ -1,25 +1,38 @@
 using IntelliTect.TestTools.Selenate;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace myWebApp.Tests
 {
     public class SmokeTests : IDisposable
     {
-        public SmokeTests()
+        public SmokeTests(ITestOutputHelper output)
         {
+            Output = output;
             var options = new ChromeOptions();
             options.AddArgument("--headless");
-            Browser = new Browser(new ChromeDriver(Path.Combine("."), options));
+            Output.WriteLine("Launching Chrome...");
+            //string chromeDriverLocation = Path.Combine(".");
+            string chromeDriverLocation = "C:\\app";
+            Output.WriteLine($"Search location for Chromedriver: {chromeDriverLocation}");
+            IWebDriver driver = new ChromeDriver(chromeDriverLocation);
+            IWebDriver driver2 = new ChromeDriver(chromeDriverLocation, options);
+            //Browser = new Browser(new ChromeDriver(Path.Combine("."), options));
+            //Browser = new Browser(new ChromeDriver("C:\\app", options));
             Browser.Driver.Navigate().GoToUrl("http://localhost:5000");
         }
 
         [Fact]
         public void VerifySiteLaunches()
         {
+            Output.WriteLine("Running test VerifySiteLaunches...");
             Assert.Contains("Hello, world!", Browser.Driver.PageSource);
+            Output.WriteLine("Test passes!");
         }
 
         public void Dispose()
@@ -28,5 +41,6 @@ namespace myWebApp.Tests
         }
 
         private Browser Browser { get; }
+        private ITestOutputHelper Output { get; }
     }
 }
